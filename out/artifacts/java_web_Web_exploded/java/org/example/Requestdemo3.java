b912b7e1aef2a62b998b94c0f436d4d55344ae3f
yaqian0616 <3045099807@qq.com>
2024-09-16 21:55:15 +0800

    first comit

---
+++ out/artifacts/java_web_Web_exploded/java/org/example/Requestdemo3.java
@@ -0,0 +1,33 @@
+package org.example;
+
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+@WebServlet("/requestdemo03")
+public class Requestdemo3 extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String header=request.getHeader("User-Agent");
+        System.out.println("header");
+        if (header.toLowerCase().contains("msie")) {
+            System.out.println("You are using Internet Explorer");
+        }else if(header.toLowerCase().contains("firefox")){
+            System.out.println("You are using the browser Firefox");
+        }else if(header.toLowerCase().contains("chrome")){
+            System.out.println("You are using Google Chrome");
+        }else  {
+            System.out.println("You're using a different browser");
+        }
+
+
+
+    }
+
+}
---
+++ request/src/main/java/org/example/Requestdemo2.java
@@ -0,0 +1,31 @@
+
+
+    package org.example;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+    @WebServlet("/requestdemo02")
+    public class Requestdemo2 extends HttpServlet {
+        @Override
+        protected void doPost(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException{}
+        @Override
+
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
+            Enumeration<String> headerNames = req.getHeaderNames();
+            while (headerNames.hasMoreElements()) {
+                String headerName = headerNames.nextElement();
+                String headerValue = req.getHeader(headerName);
+                System.out.println(headerName + ": " + headerValue);
+            }
+
+        }
+    }
+
+
